b9cc43aa6fb5eb8d55927a116f5b786443a87a8c
Kimmo Huoman <kipenroskaposti@gmail.com>
2015-10-09 11:38:39 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='siemens-pac',
-    version='0.10',
+    version='0.20',
     description='A Python library for reading Siemens SENTRON PAC -series measurement devices.',
     author='Kimmo Huoman',
     author_email='kipenroskaposti@gmail.com',
